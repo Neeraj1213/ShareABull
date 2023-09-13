befac4129a56a34ef00224516d668efb43c763ad
Neeraj <neerajpillai2004@gmail.com>
2023-09-12 22:53:00 -0400

    seperated login from appjs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import "./pages/home";
-import { createRoot } from 'react-dom/client';
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
-root.render(<App />);
+import App from './App'; 
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
